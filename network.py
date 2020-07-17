d3e57de930f25c8e1c634131df79f0777fc5c0c9
jwCheng28 <jwCheng28@users.noreply.github.com>
2020-07-16 19:25:09 -0700

    Implemented backpropagation

--- network.py
+++ network.py
@@ -13,18 +13,27 @@ class Network():
             np.random.randn(r, c + 1) 
             for r, c in zip(structure[1:], structure[:-1])
             ]
+        self.a = []
+        self.delta = []
 
     def sigmoid(self, z):
         return (1 / (1 + np.exp(-z)))
+
+    def sigmoidGrad(self, z):
+        a = self.sigmoid(z)
+        return (a * (1 - a))
 
     def forward_(self, a, l):
         return self.sigmoid(np.dot(a, self.theta[l].T))
 
     def forwardFeed(self, X):
+        self.a = []
         a = X.copy()
         for i in range(self.n_layers - 1):
             a = np.concatenate([np.ones((a.shape[0], 1)), a], axis=1)
+            self.a.append(a)
             a = self.forward_(a, i)
+        self.a.append(a)
         return a
 
     def costFunction(self, X, y):
@@ -35,7 +44,25 @@ class Network():
         J = (-1 / m) * np.sum(y * np.log(a) + (1 - y) * np.log(1 - a))
         return J 
 
-    #def back_(self, l)
+    def back_(self, l, delta):
+        a = self.a[l-1]
+        sg = self.sigmoidGrad(np.dot(a, self.theta[l-1].T))
+        return np.dot(delta, self.theta[l])[:, 1:] * sg
+
+    def backPropagation(self, y, a_l):
+        delta =  a_l - y
+        m = y.shape[0]
+        self.delta = [delta]
+        Delta = []
+        thetaGrad = []
+        for i in range(self.n_layers - 2, 0, -1):
+            delta = self.back_(i, delta)
+            self.delta.insert(0, delta)
+        for i in range(len(self.delta)):
+            Delta.append(np.dot(self.delta[i].T, self.a[i]))
+        for i in range(len(self.theta)):
+            thetaGrad.append((1 / m) * Delta[i]) 
+        return thetaGrad
 
 #n = Network([2, 3, 1])
 
