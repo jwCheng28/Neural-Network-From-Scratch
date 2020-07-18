688244224a25f4549f38b285e85022868e24340d
jwCheng28 <jwCheng28@users.noreply.github.com>
2020-07-18 16:48:16 -0700

    Added regularization:

--- network.py
+++ network.py
@@ -38,10 +38,11 @@ class Network():
         self.a.append(a)
         return a
 
-    def costFunction(self, X, y):
+    def costFunction(self, X, y, lambda_=0):
         m = y.shape[0]
         a = self.forwardFeed(X)
-        J = (-1 / m) * np.sum(y * np.log(a) + (1 - y) * np.log(1 - a))
+        reg = (lambda_/m) * np.sum([np.sum(np.square(theta[:,1:])) for theta in self.theta])
+        J = (-1 / m) * np.sum(y * np.log(a) + (1 - y) * np.log(1 - a)) + reg
         return J 
 
     def back_(self, l, delta):
@@ -49,7 +50,7 @@ class Network():
         sg = self.sigmoidGrad(np.dot(a, self.theta[l-1].T))
         return np.dot(delta, self.theta[l])[:, 1:] * sg
 
-    def backPropagation(self, y, a_l):
+    def backPropagation(self, y, a_l, lambda_=0):
         delta =  a_l - y
         m = y.shape[0]
         self.delta = [delta]
@@ -62,32 +63,35 @@ class Network():
             Delta.append(np.dot(self.delta[i].T, self.a[i]))
         for i in range(len(self.theta)):
             thetaGrad.append((1 / m) * Delta[i]) 
-        return np.array(thetaGrad)
+        thetaGrad = np.array(thetaGrad)
+        for i in range(len(thetaGrad)):
+            thetaGrad[i][:, 1:] = thetaGrad[i][:, 1:] + (lambda_/m) * self.theta[i][:, 1:]
+        return thetaGrad
 
-    def gradientDescent(self, X, y, alpha, epoch, costH=False, accurH=False):
+    def gradientDescent(self, X, y, alpha, epoch, lambda_=0, costH=False, accurH=False):
         for i in range(epoch):
             h = self.forwardFeed(X)
-            if costH: print("Epoch {} : {} ".format(i, self.costFunction(X, y)))
+            if costH: print("Epoch {} : {} ".format(i, self.costFunction(X, y, lambda_)))
             if accurH: print("Epoch {} : {} ".format(i, self.accuracy(h, y)))
-            thetaGrad = self.backPropagation(y, h)
+            thetaGrad = self.backPropagation(y, h, lambda_)
             self.theta = self.theta - alpha * thetaGrad
             if not (costH or accurH): print("Epoch {} Completed".format(i))
         h = self.forwardFeed(X)
-        print("Results - Cost : {}, Accuracy : {}".format(self.costFunction(X, y), self.accuracy(h, y)))        
+        print("Results - Cost : {}, Accuracy : {}".format(self.costFunction(X, y, lambda_), self.accuracy(h, y)))        
         return h
 
-    def stochasticGD(self, X, y, alpha, epoch, batch_size, costH=False, accurH=False):
+    def stochasticGD(self, X, y, alpha, epoch, batch_size, lambda_=0, costH=False, accurH=False):
         for i in range(epoch):
             ind = random.randint(0, len(X)-batch_size)
             X_batches, y_batches = X[ind:ind+batch_size, :], y[ind:ind+batch_size, :]
             h = self.forwardFeed(X_batches)
-            if costH: print("Epoch {} : {} ".format(i, self.costFunction(X_batches, y_batches)))
+            if costH: print("Epoch {} : {} ".format(i, self.costFunction(X_batches, y_batches, lambda_)))
             if accurH: print("Epoch {} : {} ".format(i, self.accuracy(h, y_batches)))
-            thetaGrad = self.backPropagation(y_batches, h)
+            thetaGrad = self.backPropagation(y_batches, h, lambda_)
             self.theta = self.theta - alpha * thetaGrad
             if not (costH or accurH): print("Epoch {} Completed".format(i))
         h = self.forwardFeed(X)
-        print("Results - Cost : {}, Accuracy : {}".format(self.costFunction(X, y), self.accuracy(h, y)))        
+        print("Results - Cost : {}, Accuracy : {}".format(self.costFunction(X, y, lambda_), self.accuracy(h, y)))        
         return h
 
     def accuracy(self, h, y):
