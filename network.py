9da2a32c0af7e799877a9a460a4dbd38796ee572
jwCheng28 <jwCheng28@users.noreply.github.com>
2020-07-20 17:48:30 -0700

    Added Comments

--- network.py
+++ network.py
@@ -13,7 +13,6 @@ TO DO LIST:
     - Predict Probability Graph
 '''
 
-
 class Network():
     def __init__(self, structure):
         '''
@@ -30,16 +29,20 @@ class Network():
         self.a = []
         self.delta = []
 
+    # Sigmoid Function
     def sigmoid(self, z):
         return (1 / (1 + np.exp(-z)))
 
+    # Gradient of Sigmoid Function
     def sigmoidGrad(self, z):
         a = self.sigmoid(z)
         return (a * (1 - a))
 
+    # Pass Linear Function into Sigmoid, and Returns it
     def forward_(self, a, l):
         return self.sigmoid(np.dot(a, self.theta[l].T))
 
+    # One Feed Forward
     def forwardFeed(self, X):
         self.a = []
         a = X.copy()
@@ -50,6 +53,7 @@ class Network():
         self.a.append(a)
         return a
 
+    # Cost/Loss Function using Cross Entropy
     def costFunction(self, X, y, lambda_=0):
         m = y.shape[0]
         a = self.forwardFeed(X)
@@ -57,29 +61,37 @@ class Network():
         J = (-1 / m) * np.sum(y * np.log(a) + (1 - y) * np.log(1 - a)) + reg
         return J 
 
+    # Returns derivative of backing one layer
     def back_(self, l, delta):
         a = self.a[l-1]
         sg = self.sigmoidGrad(np.dot(a, self.theta[l-1].T))
         return np.dot(delta, self.theta[l])[:, 1:] * sg
 
+    # One Back Propagation
     def backPropagation(self, y, a_l, lambda_=0):
+        # Initial delta, delta of last layer
         delta =  a_l - y
         m = y.shape[0]
         self.delta = [delta]
         Delta = []
         thetaGrad = []
+        # Calculate delta of all layers except first
         for i in range(self.n_layers - 2, 0, -1):
             delta = self.back_(i, delta)
             self.delta.insert(0, delta)
+        # Calculate delta.dot(a) (all node val of layer) for all layers
         for i in range(len(self.delta)):
             Delta.append(np.dot(self.delta[i].T, self.a[i]))
+        # Calculate of Gradient Cost/Loss Function
         for i in range(len(self.theta)):
             thetaGrad.append((1 / m) * Delta[i]) 
         thetaGrad = np.array(thetaGrad)
+        # Add Regularization for Gradient
         for i in range(len(thetaGrad)):
             thetaGrad[i][:, 1:] = thetaGrad[i][:, 1:] + (lambda_/m) * self.theta[i][:, 1:]
         return thetaGrad
 
+    # Performs Gradient Descent
     def gradientDescent(self, X, y, alpha, epoch, lambda_=0, costH=False, accurH=False):
         for i in range(epoch):
             h = self.forwardFeed(X)
@@ -92,6 +104,7 @@ class Network():
         print("Results - Cost : {}, Accuracy : {}".format(self.costFunction(X, y, lambda_), self.accuracy(h, y)))        
         return h
 
+    # Performs Stochastic Gradient Descent
     def stochasticGD(self, X, y, alpha, epoch, batch_size, lambda_=0, costH=False, accurH=False):
         for i in range(epoch):
             ind = random.randint(0, len(X)-batch_size)
@@ -106,11 +119,13 @@ class Network():
         print("Results - Cost : {}, Accuracy : {}".format(self.costFunction(X, y, lambda_), self.accuracy(h, y)))        
         return h
 
+    # Output test performance
     def accuracy(self, h, y):
         h = np.argmax(h, axis=1)
         y = np.where(y==1)[1]
         return np.mean(h==y)
 
+    # Predict IMG
     def predict(self, X, h):
         n = np.random.randint(0, len(X))
         predict = np.argmax(h, axis=1)[n]
